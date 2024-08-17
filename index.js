7f3d754d54cff3202bd73ae4378cd2f515d0e16b
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-08-17 10:39:33 +0600

    middileware added

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(
+	cors({
+		origin: [
+			"http://localhost:5173",
+			"https://eloquent-quokka-816248.netlify.app",
+			// "https://b9a11-client-side.firebaseapp.com"
+		],
+		credentials: true,
+	})
+);
+app.use(express.json());
+app.use(cookieParser());
+
 const uri = `mongodb+srv://${process.env.S3_BUCKET}:${process.env.SECRET_KEY}@cluster0.9i3jisk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
